611164f7a0bbeb903cdaa48ef13e5a3510dee4d1
tooooofu24 <toya24xxx@i.softbank.jp>
2024-08-12 17:07:04 +0900

    fix: 修正

--- lib/screens/member_setting/member_setting_screen.dart
+++ lib/screens/member_setting/member_setting_screen.dart
@@ -15,62 +15,64 @@ class MemberSettingScreen extends ConsumerWidget {
 
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: FractionallySizedBox(
-              widthFactor: 0.9,
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Spacer(),
-                    const Text('プレイヤーの名前を入力して下さい'),
-                    const SizedBox(height: 30),
-                    ...List.generate(
-                      game.players.length,
-                      (index) {
-                        final player = game.players[index];
-                        return ListTile(
-                          leading: Image(
-                            image: AssetImage(player.color.pirateImage),
-                          ),
-                          title: TextFormField(
-                            onChanged: (value) => {},
-                            validator: controller.validatePlayerName,
-                            decoration: InputDecoration(
-                              hintText: '名前を入力して下さい',
-                              labelText: '${player.color.label}色の海賊',
+        body: Padding(
+          padding: const EdgeInsets.only(bottom: 15),
+          child: SafeArea(
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Spacer(),
+                      const Text('プレイヤーの名前を入力して下さい'),
+                      const SizedBox(height: 30),
+                      ...List.generate(
+                        game.players.length,
+                        (index) {
+                          final player = game.players[index];
+                          return ListTile(
+                            leading: Image(
+                              image: AssetImage(player.color.pirateImage),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                    const Spacer(),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FilledButton.tonal(
-                        onPressed: () => controller.goBack(context),
-                        child: const Text('戻る'),
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FilledButton(
-                        onPressed: () async {
-                          if (formKey.currentState != null &&
-                              formKey.currentState!.validate()) {
-                            await controller.onSubmit(context, ref);
-                          }
+                            title: TextFormField(
+                              onChanged: (value) => {},
+                              validator: controller.validatePlayerName,
+                              decoration: InputDecoration(
+                                hintText: '名前を入力して下さい',
+                                labelText: '${player.color.label}色の海賊',
+                              ),
+                            ),
+                          );
                         },
-                        child: const Text('スタート'),
                       ),
-                    ),
-                    const SizedBox(height: 75),
-                  ],
+                      const Spacer(),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FilledButton.tonal(
+                          onPressed: () => controller.goBack(context),
+                          child: const Text('戻る'),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FilledButton(
+                          onPressed: () async {
+                            if (formKey.currentState != null &&
+                                formKey.currentState!.validate()) {
+                              await controller.onSubmit(context, ref);
+                            }
+                          },
+                          child: const Text('スタート'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
